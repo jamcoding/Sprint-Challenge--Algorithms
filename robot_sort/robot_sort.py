06ca8bbf01a41ef3a6a31a74ca2fbc846e4cb596
Alex Magana <46731475+jamcoding@users.noreply.github.com>
2020-08-14 22:47:29 -0500

    stuck on robot_sort.py

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,8 +97,13 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
-
+        while self.can_move_right():
+            self.swap_item()
+            self.move_right()
+            if self.compare_item() == 1:
+                self.swap_item()
+            self.move_left()
+            self.swap_item()
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
